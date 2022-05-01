6edc0f00b82db0c69f3357836a20073d9d2260b9
shofiq_loxodon <shofiqul.islam@egeneration.co>
2022-05-01 07:44:08 +0600

    first commit

---
+++ src/test/java/com/shofq/springcache/SpringCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shofq.springcache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
